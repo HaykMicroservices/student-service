06981de8c66430058f4dd723eadb02e38ccd39b6
hayk <haykk2018@gmail.com>
2022-10-13 14:31:44 +0400

    we used to do it by ${address.service.url} it take value from config file (address.service.url=http://localhost:8082/api/address) >but now we do it @FeignClient(value = "address-service",	path = "/api/address") instead @FeignClient(url="${address.service.url}", value="anyName")

--- src/main/java/com/infybuzz/app/StudentServiceApplication.java
+++ src/main/java/com/infybuzz/app/StudentServiceApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -17,12 +15,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableFeignClients("com.infybuzz.feignclients")
 public class StudentServiceApplication {
 
-    @Value("${address.service.url}")
-    private String addressServiceUrl;
-
     public static void main(String[] args) {
         SpringApplication.run(StudentServiceApplication.class, args);
     }
-
 }
 
--- src/main/java/com/infybuzz/feignclients/AddressFeignClient.java
+++ src/main/java/com/infybuzz/feignclients/AddressFeignClient.java
@@ -5,9 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url="${address.service.url}", value="address-feign-client")
+@FeignClient(value = "address-service", path = "/api/address")
 public interface AddressFeignClient {
 
     @GetMapping("getById/{id}")
-    public AddressResponse getById (@PathVariable long id);
+    AddressResponse getById(@PathVariable long id);
 }
